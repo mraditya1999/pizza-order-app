402efba9c52412fc2b94104b300091af8e84ad2a
mraditya1999 <ay5480620@gmail.com>
2023-03-26 12:48:44 +0530

    created cart page,auth

--- server.js
+++ server.js
@@ -5,15 +5,34 @@ const path = require('path');
 const expressEjsLayouts = require('express-ejs-layouts');
 const PORT = process.env.PORT || 3000;
 
+// Middlewares
+app.use(express.static('public'));
+app.use(expressEjsLayouts);
+
+// Views
+app.set('views', path.join(__dirname, '/resources/views'));
+app.set('view engine', 'ejs');
+
+// Routes
 app.get('/', (req, res) => {
   res.render('home');
 });
 
-// set Template engine
-app.use(express.static('public'));
-app.use(expressEjsLayouts);
-app.set('views', path.join(__dirname, '/resources/views'));
-app.set('view engine', 'ejs');
+app.get('/cart', (req, res) => {
+  res.render('customers/cart');
+});
+
+app.get('/login', (req, res) => {
+  res.render('auth/login');
+});
+
+app.get('/register', (req, res) => {
+  res.render('auth/register');
+});
+
+app.get('/forget', (req, res) => {
+  res.render('auth/forget');
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
